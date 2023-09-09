7e34cd520eed4c157c1ee98fa9a22fc569c460fa
Rafael <rafael.augsumene@gmail.com>
2023-09-09 19:15:51 -0300

    teste2 .1

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,12 +1,23 @@
-const calculadora = prompt("Qual vai ser a operação que você vai realizar hoje?\n 1- Adição +\n 2- Subtração -\n 3- Multiplicação *\n 4- Divisão /\n 5- Divisão real %\n 6- Potenciação **")
 let resultado;
 let num1 = parsefloat(document.getElementById("num1").value);
 let num2 = parseFloat(document.getElementById("num2").value);
 
 function Adicao () {
     resultado = num1 + num2;
-    
+    alert(`${resultado}`)
 }
 function Subtracao () {
     resultado = num1 - num2;
+}
+function Multiplicao () {
+    resultado = num1 * num2;
+}
+function Divisao () {
+    resultado = num1 / num2;
+}
+function  Restodadivisao () {
+    resultado = num1 / num2;
+}
+function Potenciacao () {
+    resultado = num1 ** num2;
 }
