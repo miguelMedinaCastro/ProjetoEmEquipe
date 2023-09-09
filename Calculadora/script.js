a76bd9864611e3742e192a2b022ca5e60c61f408
Rafael <rafael.augsumene@gmail.com>
2023-09-09 20:15:32 -0300

    Calculadora funcionando

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -13,31 +13,31 @@ function Subtracao () {
     resultado = num1 - num2
     alert(`${resultado}`)
     }
-    function Multiplicacao () {
+    function Multiplicao () {
         let resultado = parseFloat(document.getElementById("+"))
         let num1 = parseFloat(document.getElementById("num1").value);
         let num2 = parseFloat(document.getElementById("num2").value);
-        resultado = num1 + num2
+        resultado = num1 * num2
         alert(`${resultado}`)
         }
         function Divisao () {
             let resultado = parseFloat(document.getElementById("+"))
             let num1 = parseFloat(document.getElementById("num1").value);
             let num2 = parseFloat(document.getElementById("num2").value);
-            resultado = num1 + num2
+            resultado = num1 / num2
             alert(`${resultado}`)
             }
             function Restodadivisao () {
                 let resultado = parseFloat(document.getElementById("+"))
                 let num1 = parseFloat(document.getElementById("num1").value);
                 let num2 = parseFloat(document.getElementById("num2").value);
-                resultado = num1 + num2
+                resultado = num1 % num2
                 alert(`${resultado}`)
                 }
                 function Potenciacao () {
                     let resultado = parseFloat(document.getElementById("+"))
                     let num1 = parseFloat(document.getElementById("num1").value);
                     let num2 = parseFloat(document.getElementById("num2").value);
-                    resultado = num1 + num2
+                    resultado = num1 ** num2
                     alert(`${resultado}`)
                     }                
