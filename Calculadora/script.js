ea3e88e359195cbb3e9d65ef9a9b0e774ea5b155
Rafael <rafael.augsumene@gmail.com>
2023-09-09 19:48:42 -0300

    teste 2.3

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -4,6 +4,7 @@ let resultado = parseFloat(document.getElementById("+"))
 let num1 = parseFloat(document.getElementById("num1").value);
 let num2 = parseFloat(document.getElementById("num2").value);
 resultado = num1 + num2
+alert(`${resultado}`)
 }
 
 function Adicao () {
